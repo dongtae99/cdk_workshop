6d7b0bdc1ac71131bc8934e491a10b29eaf6076e
dalbok <dongtae99@gmail.com>
2024-11-21 06:31:04 +0900

    updating the lambda code with CDK Pipelines

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,8 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    my_invalid_function()
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CDK 2.0!')
+        'body': json.dumps('Hello from CDK 2.0 and CDK Pipelines!')
     }
